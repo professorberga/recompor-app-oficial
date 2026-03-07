b3daa4d41d39a0447fa65330f27381c166ef2abe
Márcio Bergamini <professorberga@gmail.com>
2026-03-07 22:44:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3668649691-5b266",
+  "appId": "1:842342263526:web:2f0bc927270ded50d62927",
+  "apiKey": "AIzaSyA9RfOe0FQO7jq21WS5JaEjVALf5RRAmAY",
+  "authDomain": "studio-3668649691-5b266.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "842342263526"
+};
